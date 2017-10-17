dec153a556fd3c3fea14339f1075046a9ff7ee28
chezhenjun110 <740102367@qq.com>
2017-10-17 22:04:40 +0800

    0

--- SVM/test.py
+++ SVM/test.py
@@ -1,3 +1,5 @@
-import SMO as smo
+from SVM import SMO as smo
 dataset,lables=smo.loadDataSet("testSet.txt")
-print(lables)
+b,alphas=smo.smoSimple(dataset,lables,0.6,0.001,40)
+print(b)
+print(alphas)
