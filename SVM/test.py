17167a64032362c4c8fa8922093f635fadfcfd11
chezhenjun110 <740102367@qq.com>
2017-11-06 21:59:08 +0800

    0

--- SVM/test.py
+++ SVM/test.py
@@ -1,5 +1,14 @@
 from SVM import SMO as smo
+from numpy import  *
+
 dataset,lables=smo.loadDataSet("testSet.txt")
-b,alphas=smo.smoSimple(dataset,lables,0.6,0.001,40)
-print(b)
-print(alphas)
+
+# b,alphas=smo.smoSimple(dataset,lables,0.6,0.001,40)
+# print(b)
+# print(alphas)
+# b,alphas=smo.smoP(dataset,lables,0.6,0.001,40)
+# ws=smo.calcWs(alphas,dataset,lables)
+# predict=mat(dataset)[2]*mat(ws)+b
+# print(dataset[2])
+# print(predict)
+smo.testDigits(('rbf',20))
